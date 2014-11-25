50684fd6a835f8834729d595bc48641e2bdc717b
sam <sam@XT1-20141109HVS>
2014-11-25 11:30:23 +0800

    update TestThreadPool.java

--- src/main/java/net/jcip/examples/TestThreadPool.java
+++ src/main/java/net/jcip/examples/TestThreadPool.java
@@ -1,7 +1,9 @@
 package net.jcip.examples;
 
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import junit.framework.TestCase;
 
@@ -14,37 +16,39 @@ import junit.framework.TestCase;
  */
 public class TestThreadPool extends TestCase {
 
-    private final TestingThreadFactory threadFactory = new TestingThreadFactory();
+	private final TestingThreadFactory threadFactory = new TestingThreadFactory();
 
-    public void testPoolExpansion() throws InterruptedException {
-        int MAX_SIZE = 10;
-        ExecutorService exec = Executors.newFixedThreadPool(MAX_SIZE);
+	public void testPoolExpansion() throws InterruptedException {
+		int MAX_SIZE = 10;
+		ExecutorService exec = Executors.newFixedThreadPool(MAX_SIZE,
+				threadFactory);
 
-        for (int i = 0; i < 10 * MAX_SIZE; i++)
-            exec.execute(new Runnable() {
-                public void run() {
-                    try {
-                        Thread.sleep(Long.MAX_VALUE);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            });
-        for (int i = 0;
-             i < 20 && threadFactory.numCreated.get() < MAX_SIZE;
-             i++)
-            Thread.sleep(100);
-        assertEquals(threadFactory.numCreated.get(), MAX_SIZE);
-        exec.shutdownNow();
-    }
+		for (int i = 0; i < 10 * MAX_SIZE; i++)
+			exec.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Thread.sleep(Long.MAX_VALUE);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+						System.out.println(e);
+					}
+				}
+			});
+		for (int i = 0; i < 20 && threadFactory.numCreated.get() < MAX_SIZE; i++)
+			Thread.sleep(100);
+		assertEquals(threadFactory.numCreated.get(), MAX_SIZE);
+		exec.shutdownNow();
+	}
 }
 
 class TestingThreadFactory implements ThreadFactory {
-    public final AtomicInteger numCreated = new AtomicInteger();
-    private final ThreadFactory factory = Executors.defaultThreadFactory();
+	public final AtomicInteger numCreated = new AtomicInteger();
+	private final ThreadFactory factory = Executors.defaultThreadFactory();
 
-    public Thread newThread(Runnable r) {
-        numCreated.incrementAndGet();
-        return factory.newThread(r);
-    }
+	@Override
+	public Thread newThread(Runnable r) {
+		numCreated.incrementAndGet();
+		return factory.newThread(r);
+	}
 }
